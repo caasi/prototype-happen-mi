8786c61666fa61f0bbed42ce087bace9f8455a3e
caasi Huang <caasi.igd@gmail.com>
2014-03-17 21:08:40 +0800

    fill it with steves

--- main.js
+++ main.js
@@ -9,9 +9,48 @@
       img: 'img/steve.jpg',
       title: 'Another Steve',
       desc: 'Well, Steve again.'
+    }, {
+      img: 'img/steve.jpg'
+    }, {
+      img: 'img/steve.jpg'
+    }, {
+      img: 'img/steve.jpg'
+    }, {
+      img: 'img/steve.jpg'
+    }, {
+      img: 'img/steve.jpg'
+    }, {
+      img: 'img/steve.jpg'
+    }, {
+      img: 'img/steve.jpg'
+    }, {
+      img: 'img/steve.jpg'
+    }, {
+      img: 'img/steve.jpg'
+    }, {
+      img: 'img/steve.jpg'
+    }, {
+      img: 'img/steve.jpg'
+    }, {
+      img: 'img/steve.jpg'
+    }, {
+      img: 'img/steve.jpg'
+    }, {
+      img: 'img/steve.jpg'
+    }, {
+      img: 'img/steve.jpg'
+    }, {
+      img: 'img/steve.jpg'
     }
   ];
   angular.module('mi', []).controller('MiController', ['$scope'].concat(function($scope){
+    var i$, ref$, len$, people;
+    for (i$ = 0, len$ = (ref$ = data).length; i$ < len$; ++i$) {
+      people = ref$[i$];
+      people.style = {
+        backgroundImage: "url(" + people.img + ")"
+      };
+    }
     return $scope.data = data;
   }));
 }).call(this);
--- src/ls/main.ls
+++ src/ls/main.ls
@@ -1,3 +1,5 @@
 angular.module \mi []
   .controller \MiController <[$scope]> ++ ($scope) ->
+    for people in data
+      people.style = background-image: "url(#{people.img})"
     $scope.data = data
